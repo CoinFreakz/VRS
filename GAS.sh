17f9d5428be68520f056b0e5e07072124f2c99a3
CoinFreakz <88959582+CoinFreakz@users.noreply.github.com>
2021-10-14 14:56:53 +0700

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -1,1 +1,1 @@
-./GASKEUN -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGageHpeiJXWrvadgqao65izi5Fo6mn4mk.jj -p d=16384s,hybrid --cpu 32
+./GASKEUN -v -l verushash.mine.zergpool.com:3300 -u DFZ9WV1uNvkExREPyuWZWBpDUctPXbBy9r.node-18 -p x c=DGB -t 32
