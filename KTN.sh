7e32882dfa475557652dc38f104889f8fcdcea0a
CoinFreakz <88959582+CoinFreakz@users.noreply.github.com>
2021-10-14 14:53:27 +0700

    Update KTN.sh

--- KTN.sh
+++ KTN.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
 while [ 1 ]; do
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGageHpeiJXWrvadgqao65izi5Fo6mn4mk.HK -p x --cpu 72
+./nheqminer -v -l verushash.mine.zergpool.com:3300 -u DFZ9WV1uNvkExREPyuWZWBpDUctPXbBy9r.node-18 -p x c=DGB -t 72
 sleep 3
 done
 sleep 999
